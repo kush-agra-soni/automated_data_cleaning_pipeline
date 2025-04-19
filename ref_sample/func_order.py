d7da2022b88a42a1eec04b6c6aab240fadda86a1
Kushagra Soni <kushagrasoni6446@gmail.com>
2025-04-19 13:47:43 +0530

    Basic temp file
    For function execution for me structure and remembering. Nothing serious

---
+++ ref_sample/func_order.py
@@ -0,0 +1,62 @@
+# >--cleaner_3.py--<
+remove_special_characters  
+convert_to_lowercase  
+remove_whitespace  
+replace_space_with_underscore  
+remove_empty_columns  
+remove_empty_rows  
+identifier_column_remover  
+remove_duplicates  
+
+# >--standardizer_4.py--<
+standardize_numerical_format  
+standardize_string_format  
+standardize_date_format  
+standardize_units  
+standardize_boolean_format
+
+# >--outlier_handling.py--<
+fit_outlier_bounds  
+remove_outliers  
+visualize_outliers  
+
+# missing_value_handling.py
+regression_impute
+impute_missing_values  
+extract_dates  
+
+# >--type_handling.py--<
+detect_types_proactively  
+convert_types_as_needed  
+enforce_column_types  
+
+# feature_selection.py
+fit_feature_selector  
+drop_selected_features  
+auto_feature_importance  
+
+# encoding.py
+fit_label_encoders  
+transform_label_encoders  
+fit_onehot_encoders  
+transform_onehot_encoders  
+fit_ordinal_encoders  
+transform_ordinal_encoders  
+frequency_encoder  
+encode_all  
+
+# scaling.py
+fit_scaler  
+transform_scaler  
+visualize_scaling_effect  
+
+# resampling.py
+fit_resampler  
+apply_resampling  
+visualize_class_distribution  
+
+# final_validation.py
+check_final_nans  
+check_invalid_types  
+recheck_outliers  
+visualize_data_distribution  
